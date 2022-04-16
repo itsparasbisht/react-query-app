25787705a84d12d3adec3bb0084b779137969b40
itsparasbisht <iamparasbisht@gmail.com>
2022-04-16 19:56:41 +0530

    error handling

--- src/components/RQSuperHeroes.js
+++ src/components/RQSuperHeroes.js
@@ -7,10 +7,13 @@ const fetchSuperHeroes = () => {
 }
 
 export const RQSuperHeroes = () => {
-    const {isLoading, data} = useQuery('super-heroes', fetchSuperHeroes)
+    const {isLoading, data, isError, error} = useQuery('super-heroes', fetchSuperHeroes)
 
     if(isLoading){
-        return <h1>Loading...</h1>
+        return <h2>Loading...</h2>
+    }
+    if(isError){
+        return <h2>{error.message}</h2>
     }
 
   return (
