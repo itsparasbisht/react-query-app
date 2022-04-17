1887c6e384312c2140dc7edc36f98c73f3649873
itsparasbisht <iamparasbisht@gmail.com>
2022-04-17 19:41:37 +0530

    stale time

--- src/components/RQSuperHeroes.js
+++ src/components/RQSuperHeroes.js
@@ -8,7 +8,8 @@ const fetchSuperHeroes = () => {
 
 export const RQSuperHeroes = () => {
     const {isLoading, data, isError, error, isFetching} = useQuery('super-heroes', fetchSuperHeroes, {
-        cacheTime: 5000
+        // cacheTime: 5000,
+        staleTime: 30000
     })
 
     console.log(isLoading, isFetching)
